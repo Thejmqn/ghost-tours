2f425f9bda4b30abfe9c2a11e70b1067050b1356
TheJmqn <jman4461@gmail.com>
2025-02-13 21:22:34 -0500

    Update branding

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import logo from './logo.svg';
+import ghost from './ghost.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} style={{height: "16vh"}} className="App-logo" alt="logo" />
         <p>
-          Ghost Tours
+          Explore the Haunted Grounds of Mr. Jefferson's University
         </p>
+        <img src={ghost} style={{height: "20vh"}} className="Ghost-image" alt="A ghost" />
       </header>
+      <footer className="App-footer">
+         Disclaimer: Ghosts on Grounds is not sponsored or affiliated in any way with the University of Virginia.
+      </footer>
     </div>
   );
 }
