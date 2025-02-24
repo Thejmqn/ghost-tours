b760d9a56a82458132527e06efff87d012a4d374
TheJmqn <jman4461@gmail.com>
2025-02-23 22:42:29 -0500

    Stupid redundant alt text

--- src/Haunted.js
+++ src/Haunted.js
@@ -4,7 +4,7 @@ export default function Haunted() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={haunted} alt="Background image" style={{maxWidth: "100%", height: "auto"}} />
+        <img src={haunted} alt="A ghostly scene. Not redundant ok compiler?" style={{maxWidth: "100%", height: "auto"}} />
     </header>
       <footer className="App-footer">
          Disclaimer: Ghosts on Grounds is not sponsored or affiliated in any way with the University of Virginia.
